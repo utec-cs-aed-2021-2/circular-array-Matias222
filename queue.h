06345c7e7db42b8b71e8b14ce16b16bc0c6a97db
Matias222 <74629902+Matias222@users.noreply.github.com>
2021-09-09 19:05:31 -0500

    Update queue.h

--- queue.h
+++ queue.h
@@ -2,5 +2,10 @@
 
 template <typename T>
 class QueueArray : public CircularArray<T> { 
-    
+    public:
+        void push(T data){ this->push_back(data);}
+        T pop(){
+            T temp=this->pop_front();
+            return temp;
+        }
 };
